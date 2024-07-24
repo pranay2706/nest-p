e415ab8d7929ff0877efd7cfaf27801353fd1040
pranay2706 <pranay.dak@infoobjects.com>
2024-07-24 10:43:56 +0530

    not required

--- admin/dist/product/product.controller..d.ts
+++
@@ -1,9 +0,0 @@
-import { ProductService } from './product.service';
-import { Product } from './product.entity';
-export declare class ProductController {
-    private productService;
-    constructor(productService: ProductService);
-    all(): Promise<Product[]>;
-    create(body: any): Promise<Product>;
-    get(id: number): Promise<Product>;
-}
--- admin/dist/product/product.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from './product.entity';
-import { Repository } from 'typeorm';
-export declare class ProductService {
-    private readonly productRepository;
-    constructor(productRepository: Repository<Product>);
-    all(): Promise<Product[]>;
-    create(data: any): Promise<Product>;
-    get(id: number): Promise<Product>;
-}
